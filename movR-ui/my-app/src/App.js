f12c648be4b982bcc0ec4e308dd12af8f2dce727
Anas Shahid <muhammad.shahid@ericsson.com>
2021-10-30 03:40:40 -0400

    added chat component

--- movR-ui/my-app/src/App.js
+++ movR-ui/my-app/src/App.js
@@ -5,6 +5,7 @@ import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
 import MapComponent from './components/Map/MapComponent.jsx'
 import HomePage from './components/Home/HomePage.jsx'
+import Chat from './components/Chat/Chat';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
             <Route path="/signup" component={Signup} />
             <Route path="/map" component={MapComponent} />
             <Route path="/home" component={HomePage} />
+            <Route path="/chat" component={Chat} />
             <Redirect exact from="/" to="/login" />
           </Switch>
         </Router>
