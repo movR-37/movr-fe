95e2613e237341222142aa37c903ff9bcf784c33
hadi <hadizia6@gmail.com>
2021-11-21 22:47:51 -0500

    form without padding

--- movR-ui/my-app/src/components/Form.jsx
+++ movR-ui/my-app/src/components/Form.jsx
@@ -5,6 +5,11 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormLabel from '@material-ui/core/FormLabel';
+import TextField from '@material-ui/core/TextField';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
 import App from '../App';
 
 
@@ -24,15 +29,40 @@ export default function Form() {
     }
         return( 
             <form>
-                <input placeholder='Expected Time/ hr' 
+                {/* <TextField placeholder='Expected Time/ hr' 
                 value = {estimatedTime} 
-                onChange={e => setEstimatedTime(e.target.value)}>
-                </input>
+                onChange={e => setEstimatedTime(e.target.value)}
+                >
+                </TextField> */}
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <OutlinedInput
+                id="outlined-adornment-Time"
+                value={estimatedTime}
+                size="small"
+                onChange={e => setEstimatedTime(e.target.value)}
+                endAdornment={<InputAdornment position="end">hr</InputAdornment>}
+                aria-describedby="outlined-Time-helper-text"
+                inputProps={{
+                'aria-label': 'Time',
+                }}
+                />
+                <FormHelperText id="outlined-Distance-helper-text">Time</FormHelperText>
+                </FormControl>
                 <br/>
-                <input placeholder='Expected Distance/ mi' 
-                value = {estimatedDistance} 
-                onChange={e => setEstimatedDistance(e.target.value)}>
-                </input>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <OutlinedInput
+                id="outlined-adornment-Distance"
+                size="small"
+                value={estimatedDistance}
+                onChange={e => setEstimatedDistance(e.target.value)}
+                endAdornment={<InputAdornment position="end">km</InputAdornment>}
+                aria-describedby="outlined-Distance-helper-text"
+                inputProps={{
+                'aria-label': 'Distance',
+                }}
+                />
+                <FormHelperText id="outlined-Distance-helper-text">Distance</FormHelperText>
+                </FormControl>
                 <br/>
                 <p>Hourly rate: 30$</p>
                 <p>Tax: 15%</p>
@@ -45,16 +75,27 @@ export default function Form() {
                  
                 <FormControl component="fieldset">
                 <FormLabel component="legend">Select Tip %</FormLabel>
-                <RadioGroup row aria-label="tip" name="row-radio-buttons-group" defaultValue="15">
+                <RadioGroup row aria-label="tip" name="row-radio-buttons-group" defaultValue="15"
+                size="small">
                 <FormControlLabel value="15" control={<Radio />} label="15" onChange={e => setCurrentTip(e.target.value)}/>
                 <FormControlLabel value="20" control={<Radio />} label="20" onChange={e => setCurrentTip(e.target.value)}/>
                 <FormControlLabel value="25" control={<Radio />} label="25" onChange={e => setCurrentTip(e.target.value)}/>
                 </RadioGroup>
                 </FormControl>
                 <br/>
-                <button onClick={(e) => onSubmit(e)}>Calculate Cost</button>
+                <Button size="medium" onClick={(e) => onSubmit(e)}>Calculate Cost</Button>
                 <br/>
-                <input type="text" id="result" value = {currentSum} readOnly placeholder='$$$'/>
+                <br/>
+                {/* <input type="text" id="result" value = {currentSum} readOnly placeholder='$$$'/> */}
+                <TextField
+                label="Estimated Cost"
+                id="result"
+                value={currentSum}
+                defaultValue="$$"
+                size="small"
+                variant="outlined"
+                onChange={e => setCurrentSum(e.target.value)}
+                />
             </form>
         );
     }
