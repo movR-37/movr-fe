6ebaf16efdcbdbe98f3d0f6ddc55a09085e4b2c6
sanihaseeb <sanihaseeb@hotmail.com>
2021-11-24 14:56:43 -0500

    Add css

--- movR-ui/my-app/src/components/MoverProfile/MoverProfile.jsx
+++ movR-ui/my-app/src/components/MoverProfile/MoverProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import userLogo from "../../images/user.png";
 import moverLogo from "../../images/mover.png";
+import './MoverProfile.css';
 
 
 export default function MoverProfile() {
@@ -8,13 +9,13 @@ export default function MoverProfile() {
 
     return (
         <div className="limiter">
-            <div className="container-landing100">
-                <div className="wrap-landing100">
+            <div className="container-moverProfile100">
+                <div className="wrap-moverProfile100">
                     <div>
                         <img src={userLogo} width="350" height="350" alt="IMG" />
                     </div>
-                    <div className="login100-form validate-form">
-                        <span className="login100-form-title"> My Profile </span>
+                    <div className="moverProfile100-form validate-form">
+                        <span className="moverProfile100-form-title"> My Profile </span>
                         <div className="form-input-container">
                             <div className="wrap-input100 validate-input">
                                 <input
@@ -35,12 +36,48 @@ export default function MoverProfile() {
                                 />
                             </div>
 
-                            <div className="wrap-input100 validate-input">
+                            <div>
 
-                                <input
-                                    className="input100"
-                                    placeHolder="A link to your image"
-                                />
+                                <div className="wrap-input100 validate-input">
+
+                                    <input
+                                        className="input100"
+                                        placeHolder="A link to your image"
+                                    />
+
+                                </div>
+                                <div className="wrap-input100 validate-input">
+
+                                    <input
+                                        className="input100"
+                                        placeHolder="A link to your image"
+                                    />
+
+                                </div>
+                                <div className="wrap-input100 validate-input">
+
+                                    <input
+                                        className="input100"
+                                        placeHolder="A link to your image"
+                                    />
+
+                                </div>
+                                <div className="wrap-input100 validate-input">
+
+                                    <input
+                                        className="input100"
+                                        placeHolder="A link to your image"
+                                    />
+
+                                </div>
+                                <div className="wrap-input100 validate-input">
+
+                                    <input
+                                        className="input100"
+                                        placeHolder="A link to your image"
+                                    />
+
+                                </div>
 
                             </div>
                             <div className="container-login100-form-btn">
